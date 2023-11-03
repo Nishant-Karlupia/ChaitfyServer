7ee16111ecc9dac7ed5ba48ce9eaf9608ddbbe3e
Nishant Karlupia <nishantkarlupia149@gmail.com>
2023-11-03 17:30:55 +0530

    origin changed

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const server=app.listen(process.env.PORT,()=>{
 
 const io=socket(server,{
     cors:{
-        origin:"https://chatifyserver-qv99.onrender.com",
+        origin:"https://chatifyuser.netlify.app",
         credentials:true,
     },
 })
