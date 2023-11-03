1b08f7ac0d8e136236487e8bc3863957e0f7802f
Nishant Karlupia <nishantkarlupia149@gmail.com>
2023-11-03 17:28:43 +0530

    origin changed

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const PORT=process.env.PORT || 5000
 require("dotenv").config();
 
 app.use(cors());
+app.use(cors({
+    origin: 'https://chatifyuser.netlify.app',
+}));
 app.use(express.json());
 
 app.use("/api/auth",userRoutes);
